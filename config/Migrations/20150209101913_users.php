0d665c2784cfa89f543f28ee24ba6e6117683ac2
bravo-kernel <bravo-kernel@users.noreply.github.com>
2015-02-09 12:38:03 +0100

    Adds Users migration

---
+++ config/Migrations/20150209101913_users.php
@@ -0,0 +1,52 @@
+<?php
+use Phinx\Migration\AbstractMigration;
+
+class Users extends AbstractMigration {
+
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     *
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('users',
+            [
+                'id' => false,
+                'primary_key' => ['id']
+            ]);
+
+        $table
+            ->addColumn('id', 'uuid')
+            ->addColumn('username', 'string')
+            ->addColumn('email', 'string')
+            ->addColumn('password', 'string')
+            ->addColumn('slug', 'string')
+            ->addColumn('role', 'integer', ['signed' => false, 'default' => '1'])
+            ->addColumn('created', 'datetime', ['default' => 'CURRENT_TIMESTAMP'])
+            ->addColumn('modified', 'datetime', ['null' => '1', 'default' => null])
+            ->save();
+    }
+
+    /**
+     * Migrate Up.
+     *
+     * @return void
+     */
+    public function up()
+    {
+    }
+
+    /**
+     * Migrate Down.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+
+}
