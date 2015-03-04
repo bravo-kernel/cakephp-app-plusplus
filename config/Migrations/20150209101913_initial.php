8baa387fea204eb5cf141fadc8e7087b61cd7b6f
bravo-kernel <bravo-kernel@users.noreply.github.com>
2015-03-04 14:14:59 +0000

    Updates initial migration with all optional tables

--- config/Migrations/20150209101913_initial.php
+++ config/Migrations/20150209101913_initial.php
@@ -1,7 +1,7 @@
 <?php
 use Phinx\Migration\AbstractMigration;
 
-class Users extends AbstractMigration {
+class Initial extends AbstractMigration {
 
     /**
      * Change Method.
@@ -25,7 +25,23 @@ class Users extends AbstractMigration {
             ->addColumn('email', 'string')
             ->addColumn('password', 'string')
             ->addColumn('slug', 'string')
+            ->addColumn('active', 'boolean')
             ->addColumn('role_id', 'integer', ['signed' => false, 'default' => '1'])
+            ->addColumn('created', 'datetime', ['default' => 'CURRENT_TIMESTAMP'])
+            ->addColumn('modified', 'datetime', ['null' => '1', 'default' => null])
+            ->save();
+
+        $table = $this->table('user_roles')
+            ->addColumn('name', 'string')
+            ->addColumn('alias', 'string')
+            ->addColumn('description', 'text')
+            ->addColumn('created', 'datetime', ['default' => 'CURRENT_TIMESTAMP'])
+            ->addColumn('modified', 'datetime', ['null' => '1', 'default' => null])
+            ->save();
+
+        $table = $this->table('user_roles_users')
+            ->addColumn('user_role_id', 'integer', ['signed' => false])
+            ->addColumn('user_id', 'uuid')
             ->addColumn('created', 'datetime', ['default' => 'CURRENT_TIMESTAMP'])
             ->addColumn('modified', 'datetime', ['null' => '1', 'default' => null])
             ->save();
